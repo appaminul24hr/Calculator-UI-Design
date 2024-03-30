26dfd274e7f2c0c2860e59faa13f27c2b25939b7
appaminul24hr <appdeveloperaminul@gmail.com>
2024-03-30 22:08:45 +0600

    first commit

---
+++ app/src/main/java/com/aisoft/testapp/MainActivity.java
@@ -0,0 +1,40 @@
+package com.aisoft.testapp;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+//Variable Declire
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
